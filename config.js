9881e2c73c9537b9f23b41c0b85fe883c07b4061
James.Taylor <james.taylor@paypal.com>
2013-11-27 19:47:57 -0600

    Initialize settings

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // CHANGE THESE VALUES TO MATCH YOUR DEVELOPER ACCOUNT
-exports.PAYPAL_APP_ID = "MyApplicationId";
-exports.PAYPAL_SECRET = "A8VERY8SECRET8VALUE0";
+exports.PAYPAL_APP_ID = "AX6pWxCGU2t_JewkXEsHoHaOc8kPpQ_M0P5h1QcJe6Msr6gU3RFIoa71rI_u";
+exports.PAYPAL_SECRET = "EIvAahAf6yN6OJEQiYmUBcVUXMTtciMwbAfePIerVJcDsOqLMFVPps-5A_8j";
 
 // If true, all calls to the PayPal Here API will go through this sample server. See the README for
 // a discussion of this, but basically this is more secure and simpler to manage at the cost of relatively
@@ -10,9 +10,10 @@ exports.CENTRAL_SERVER_MODE = true;
 // Note that you need an entry in your hosts file for this
 // and that it must match the returnUrl set on your app in
 // PayPal Access
-exports.MY_HOSTNAME = "http://my-sample-service.herokuapp.com";
+exports.MY_HOSTNAME = "http://ancient-dawn-6273.herokuapp.com";
 exports.MY_PORT = 80;
-exports.MY_APP_URL = "sdksampleapp://oauth";
+exports.MY_APP_URL = "http://localhost:61951/return.aspx";
+//exports.MY_APP_URL = "sdksampleapp://oauth";
 
 // In case you want more info, modify this
 exports.PAYPAL_SCOPES = "https://uri.paypal.com/services/paypalhere openid email profile";
